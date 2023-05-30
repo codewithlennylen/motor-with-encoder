a3ef7b96ef04a2b73aaa74735842cc7cb3f607c6
codewithlennylen <codewithlennylen254@gmail.com>
2023-05-31 02:05:14 +0300

    Proportional Control Works

--- motor-with-encoder/src/main.cpp
+++ motor-with-encoder/src/main.cpp
@@ -37,80 +37,69 @@ void setup()
 void loop()
 {
 
-  setMotor(1, enA, motorSpeed, in1, in2);
-  delay(3000);
-  setMotor(0, enA, motorSpeed, in1, in2);
-  delay(2000);
-  setMotor(-1, enA, motorSpeed, in1, in2);
-  delay(3000);
-  setMotor(0, enA, motorSpeed, in1, in2);
-  delay(2000);
-
   // ------------ MOTOR CONTROLLER (PID) ----------------------
 
   // set target position
-  // int target = 1200;
+  int target = 1200;
   // int target = 250*sin(prevT/1e6)
 
   // PID Constants
-  // float kp = 1;
-  // float ki = 0;
-  // float kd = 0;
+  float kp = 1;
+  float ki = 0;
+  float kd = 0;
 
+  // compute time difference
+  long currT = micros();
+
+  float deltaT = ((float)(currT - prevT)) / 1e6; // deltaT in seconds
+  prevT = currT;
+
+  // calculate error
+  // int e = target - encoderPosition;
+  int e = encoderPosition - target;
+
+  // derivative
+  float dedt = (e - eprev) / (deltaT);
+
+  // integral
+  eintegral = eintegral + e * deltaT;
+
+  // calculate control signal
+  float u = kp * e + ki * eintegral + kd * dedt;
+
+  // set motor power
+  float pwr = fabs(u);
+  if (pwr > 255)
+  {
+    pwr = 255;
+  }
+
+  // set motor direction
+  int dir = 1;
+  if (u < 0)
+  {
+    dir = -1;
+  }
+
+  // signal the motor
+  setMotor(dir, enA, pwr, in1, in2);
+
+  // store previous error
+  eprev = e;
+
+  Serial.print("Target Position: ");
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.print("Measured Position: ");
   Serial.print(encoderPosition);
   Serial.print(" ");
-  // time difference
-  // long currT = micros();
-
-  // float deltaT = ((float)(currT - prevT)) / 1e6; // deltaT in seconds
-  // prevT = currT;
-
-  // // calculate error
-  // // int e = target - encoderPosition;
-  // int e = encoderPosition - target;
-
-  // // derivative
-  // float dedt = (e - eprev) / (deltaT);
-
-  // // integral
-  // eintegral = eintegral + e * deltaT;
-
-  // // calculate control signal
-  // float u = kp * e + ki * eintegral + kd * dedt;
-
-  // // set motor power
-  // float pwr = fabs(u);
-  // if (pwr > 255)
-  // {
-  //   pwr = 255;
-  // }
-
-  // // set motor direction
-  // int dir = 1;
-  // if (u < 0)
-  // {
-  //   dir = -1;
-  // }
-
-  // // signal the motor
-  // // setMotor(dir, enA, pwr, in1, in2);
-
-  // // store previous error
-  // eprev = e;
-
-  // Serial.print("Target Position: ");
-  // Serial.print(target);
-  // Serial.print(" ");
-  Serial.print("Measured Position: ");
-  Serial.println(encoderPosition);
-  // Serial.print(" ");
-  // Serial.print("Error: ");
-  // Serial.print(e);
-  // Serial.print(" ");
-  // Serial.print("Control Signal: ");
-  // Serial.print(u);
-  // Serial.print(" ");
-  // Serial.println();
+  Serial.print("Error: ");
+  Serial.print(e);
+  Serial.print(" ");
+  Serial.print("Control Signal: ");
+  Serial.print(u);
+  Serial.print(" ");
+  Serial.println();
 }
 
 void readEncoder()
@@ -133,19 +122,19 @@ void setMotor(int dir, int pwmPin, int pwmVal, int in1, int in2)
 
   if (dir == 1)
   {
-    Serial.println("Forward");
+    // Serial.println("Forward");
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
   }
   else if (dir == -1)
   {
-    Serial.println("Backward");
+    // Serial.println("Backward");
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
   }
   else
   {
-    Serial.println("Stop");
+    // Serial.println("Stop");
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
   }
