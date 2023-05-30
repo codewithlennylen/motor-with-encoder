8df0d504bc485912f7e5864458d2916677abd31d
codewithlennylen <codewithlennylen254@gmail.com>
2023-05-31 01:39:43 +0300

    Motor Test :+1:  - Faulty L298N slowed  progress

--- motor-with-encoder/src/main.cpp
+++ motor-with-encoder/src/main.cpp
@@ -1,64 +1,52 @@
 #include <Arduino.h>
-// #include <motor_encoder.h>
-#include <basic_motor.h>
+#include <motor_encoder.h>
 
 // Define pins connected the encoders A & B
-// #define ENCA 2 // Yellow
-// #define ENCB 3 // Green
+#define ENCA 2 // Yellow
+#define ENCB 3 // Green
 
 // Motor A connections
-// int enA = 10;
-// int in1 = 8;
-// int in2 = 7;
-// int motorSpeed = 230;
+int enA = 8;
+int in1 = 9;
+int in2 = 10;
+int motorSpeed = 100;
 
-// int pos = 0;
-// long prevT = 0;
-// float eprev = 0;
-// float eintegral = 0;
+// PID-related variables
+int encoderPosition = 0;
+long prevT = 0;
+float eprev = 0;
+float eintegral = 0;
 
-// int encoderPosition = 0;
-
-// void readEncoder();
-// void setMotor(int dir, int pwmPin, int pwmValue, int in1, int in2);
+void readEncoder();
+void setMotor(int dir, int pwmPin, int pwmValue, int in1, int in2);
 
 void setup()
 {
 
-  // Serial.begin(9600); // Initialize the serial port
-  // pinMode(ENCA, INPUT);
-  // pinMode(ENCB, INPUT);
-  // pinMode(enA, OUTPUT);
-  // pinMode(in1, OUTPUT);
-  // pinMode(in2, OUTPUT);
+  // Initialize the serial port
+  Serial.begin(9600);
+  pinMode(ENCA, INPUT);
+  pinMode(ENCB, INPUT);
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
 
-  motor_setup();
-  stop_all();
-  // attachInterrupt(digitalPinToInterrupt(ENCA), readEncoder, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCA), readEncoder, RISING);
 }
 
 void loop()
 {
-  go_forward();
-  // delay(5000);
-  // stop_all();
-  // delay(200);
-  // go_backwards();
-  // delay(5000);
-  // stop_all();
-  // delay(2000);
-  // Serial.println(encoderPosition);
-  // setMotor(1, enA, motorSpeed, in1, in2);
-  // delay(2000);
-  // setMotor(0, enA, motorSpeed, in1, in2);
-  // delay(1000);
-  // // Serial.println(encoderPosition);
-  // setMotor(-1, enA, motorSpeed, in1, in2);
-  // delay(2000);
-  // // Serial.println(encoderPosition);
-  // setMotor(0, enA, motorSpeed, in1, in2);
-  // delay(1000);
-  // Serial.println(encoderPosition);
+
+  setMotor(1, enA, motorSpeed, in1, in2);
+  delay(3000);
+  setMotor(0, enA, motorSpeed, in1, in2);
+  delay(2000);
+  setMotor(-1, enA, motorSpeed, in1, in2);
+  delay(3000);
+  setMotor(0, enA, motorSpeed, in1, in2);
+  delay(2000);
+
+  // ------------ MOTOR CONTROLLER (PID) ----------------------
 
   // set target position
   // int target = 1200;
@@ -69,6 +57,8 @@ void loop()
   // float ki = 0;
   // float kd = 0;
 
+  Serial.print(encoderPosition);
+  Serial.print(" ");
   // time difference
   // long currT = micros();
 
@@ -111,8 +101,8 @@ void loop()
   // Serial.print("Target Position: ");
   // Serial.print(target);
   // Serial.print(" ");
-  // Serial.print("Measured Position: ");
-  // Serial.print(encoderPosition);
+  Serial.print("Measured Position: ");
+  Serial.println(encoderPosition);
   // Serial.print(" ");
   // Serial.print("Error: ");
   // Serial.print(e);
@@ -122,7 +112,7 @@ void loop()
   // Serial.print(" ");
   // Serial.println();
 }
-/*
+
 void readEncoder()
 {
   int b = digitalRead(ENCB);
@@ -160,4 +150,3 @@ void setMotor(int dir, int pwmPin, int pwmVal, int in1, int in2)
     digitalWrite(in2, LOW);
   }
 }
-*/
